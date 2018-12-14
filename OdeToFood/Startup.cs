3e60e24ac51dac782f47c12bb444e16a7c4d845d
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2018-12-14 18:15:11 -0200

    :heavy_plus_sign: Cria o banco de dados

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -14,12 +14,19 @@ namespace OdeToFood
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddDbContext<OdeToFoodDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("OdeToFood")));
+            services.AddDbContext<OdeToFoodDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
         }
